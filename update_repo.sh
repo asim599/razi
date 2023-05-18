df988ec3374056c2167f8c2aedcfe89efd7c7c26
asim599 <h.asim599@gmail.com>
2023-05-18 09:22:11 -0400

    initial commit

---
+++ update_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REMOTE_BRANCH="master"
+LOCAL_BRANCH="master"
+REPO_PATH="/root/myrepo"
+
+cd "$REPO_PATH"
+
+git fetch
+
+LOCAL_COMMIT=$(git rev-parse "$LOCAL_BRANCH")
+
+REMOTE_COMMIT=$(git rev-parse "origin/$REMOTE_BRANCH")
+
+if [ "$LOCAL_COMMIT" = "$REMOTE_COMMIT" ]; then
+  echo "Local branch is up to date. Pulling changes..."
+  git pull origin "$REMOTE_BRANCH"
+else
+  echo "Local branch is not up to date. Skipping pull."
+fi
+
